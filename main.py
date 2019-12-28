fdc3b879aaad1fa764ed77286f7c5eaa355fd838
rossschool <tess@ross.org>
2019-12-28 03:30:41 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,37 @@
+'''
+Feel free to play with the code here, experiment!
+'''
+
+print('What is the type of an integer in Python?')
+print(27)
+print(type(27))
+print()
+
+
+
+print('What is the type of a float in Python?')
+print(13.5)
+print(type(13.5))
+print()
+
+
+
+print('Can you explain why the output doesn\'t match the value in the print statement?')
+print(2.66666666666666666666666666)
+print(type(2.66666666666666666666666666))
+
+
+
+print('Notice the outputs of these three print statements')
+print(7 == 7)
+print(3 == 4)
+print(type(3 == 4))
+
+
+
+
+
+'''
+Galvanize Data Science Prep
+https://www.galvanize.com/data-science/prep
+'''
